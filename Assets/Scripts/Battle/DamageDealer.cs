67669c78bb5eaccf3dac1a12cb819137613fd352
Насибуллин Оскар Рамилевич <oscarn08@gmail.com>
2019-05-10 17:33:14 +0300

    Landscape mode for battle scene

--- Assets/Scripts/Battle/DamageDealer.cs
+++ Assets/Scripts/Battle/DamageDealer.cs
@@ -7,10 +7,20 @@ namespace Battle
 {
 public class DamageDealer : MonoBehaviour
 {
-
+	[SerializeField] private CanvasGroup _puzzleCanvas;
 	[SerializeField] private Character _attackTarget;
+	[SerializeField] private float _alpha = 0.2f;
 	[SerializeField] private int _damage;
 
+	public void makeCanvasTransparent(){
+		_puzzleCanvas.gameObject.SetActive(false);
+		// _puzzleCanvas.alpha = _alpha;
+	}
+
+	public void makeCanvasVisible(){
+		_puzzleCanvas.gameObject.SetActive(true);
+		// _puzzleCanvas.alpha = 1;
+	}
 
 	public void setDamage(int damage){
 		_damage = damage;
